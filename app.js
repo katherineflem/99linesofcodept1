bfa40a9056e84ad1da9306db52ece391642d6f24
katherineflem <katherinemarief@gmail.com>
2019-02-23 11:57:53 -0600

    99lines of code pt 1 almost completed

---
+++ app.js
@@ -0,0 +1,6 @@
+let friends = ['George', 'Audrey', 'Emily', 'MM', 'Conrad']
+for (var i = 0; i < friends.length; i++) {
+    for (var a = 99; a > 0; a--) {
+        console.log([a] + "lines of code in the file," + [a] + "lines of code." + friends[i] + "strikes one out,clears it all out" + [a] + "lines of code in the file.")
+    }
+}
